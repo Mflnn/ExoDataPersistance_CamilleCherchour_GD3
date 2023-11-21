24c36a3647cfe0c6015bd71803854fd084e5f15f
CHERCHOUR Camille <camille.cherchour@brassart.fr>
2023-11-21 23:16:45 +0100

    Quete Start
    Force les bool IsStarted et IsFinished au lancement

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private ScoreDatas _ScoreData;
+    [SerializeField] private Quest _quete;
 
     private void Start()
     {
         _ScoreData.InitScoreValue(0);
+        _quete.IsStarted = false;
+        _quete.IsFinished = false;
     }
 }
